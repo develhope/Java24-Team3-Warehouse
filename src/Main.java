7e5682d3e61c445a94f182c8210350407a82aab6
Chiara De Guglielmo <154986750+CDG01@users.noreply.github.com>
2024-03-25 19:10:41 +0100

    feat: class Itam e customerMeru()

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,26 @@ public class Main {
 
     }
 
+    public static void customerMenu() {
+        System.out.println("""
+                    MENU' CUSTOMER:
+                    1) Carrello
+                    2) Catalogo
+                    3) logout
+                    seleziona una opzione inserendo il suo numero:
+                """);
+
+        while (true) {
+            switch (inputInt()) {
+                case 1:
+                    cartMenu();
+                case 2:
+                    menuCatalogue();
+                case 3:
+                    Login.readString();
+                default:
+                    System.out.println("Valore non valido, riprova.");
+            }
+        }
+    }
 }
