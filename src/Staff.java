fc260740d237ca17c1f3c212a1d89dcd51bb5a02
davidedileo <dudeda94@gmail.com>
2024-03-26 17:11:58 +0100

    add feature/login

---
+++ src/Staff.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Staff extends User{
+
+
+    public Staff(int userId, String username, String password) {
+        super(userId, username, password);
+    }
+
+
+}
